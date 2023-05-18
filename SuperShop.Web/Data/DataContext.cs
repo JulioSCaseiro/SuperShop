e13c5de4c7e325077793a142b5aba882270e0f13
JulioSCaseiro <caseiroinc@gmail.com>
2023-05-18 14:32:24 +0100

    Criacao nova Framework + First Controller

---
+++ SuperShop.Web/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop.Web.Data.Entities;
+
+namespace SuperShop.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
--- SuperShop/SuperShop.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SuperShop.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
