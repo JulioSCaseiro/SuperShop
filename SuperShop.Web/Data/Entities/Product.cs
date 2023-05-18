72365eb63a21c4cbb5633523931dd1eb2e9ad798
JulioSCaseiro <caseiroinc@gmail.com>
2023-05-18 15:37:41 +0100

    Modify Products.

--- SuperShop.Web/Data/Entities/Product.cs
+++ SuperShop.Web/Data/Entities/Product.cs
@@ -6,15 +6,17 @@ namespace SuperShop.Web.Data.Entities
     public class Product
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
         public decimal Price { get; set; }
         [Display(Name = "Image")]
         public string ImageURL { get; set; }
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
         [Display(Name = "Is Available")]
         public bool IsAvailable { get; set; }
         [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
